f134abc8683d3e2372a30652d6e6604ea1c7d713
liangxiaowei <xiaoweiliang@caixinmedia.com>
2013-03-12 18:44:11 +0800

    first commit

---
+++ DiyDdp/Classes/Logic/DDGameLevel.h
@@ -0,0 +1,13 @@
+//
+//  DDGameLevel.h
+//  DiyDdp
+//
+//  Created by xiaowei on 13-3-12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDGameLevel : NSObject
+
+@end
---
+++ DiyDdp/Classes/Logic/DDGameLevel.m
@@ -0,0 +1,13 @@
+//
+//  DDGameLevel.m
+//  DiyDdp
+//
+//  Created by xiaowei on 13-3-12.
+//
+//
+
+#import "DDGameLevel.h"
+
+@implementation DDGameLevel
+
+@end
